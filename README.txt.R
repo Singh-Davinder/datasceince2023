ee2c1432dc8e2abeefc7cab51261d7dfe971a39a
Davinder Singh <davinderhothi2000@gmail.com>
2023-02-07 15:51:37 -0800

    commit

---
+++ README.txt.R
@@ -0,0 +1,16 @@
+General information
+1. Title of the Project: Data science Research in Biology Course 2023
+2. Name/instiution/address of the author or person resposible for the project
+3. Date the project started
+4. keywords: Data science, R
+5. Programming languages: R
+6. Funding sources
+7. Lecense: CC-BY
+
+File overview
+Name of folder: Description of folder (what does it contain?)
+Name on file: Description
+folder figs: contain figure file of analysis run on the scripts folder
+folder data-raw: contain raw data files
+scripts: scripts for the data
+data: data that has been analysed and changed
